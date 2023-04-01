629b05289d7006de26edc10e386ac6f12963a7d1
Ümit Can HAN <79011785+umit-han@users.noreply.github.com>
2023-04-02 01:02:10 +0300

    feat: adding search region

--- components/SearchRegion.vue
+++ components/SearchRegion.vue
@@ -6,9 +6,9 @@
                     <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                         <svg aria-hidden="true" class="w-5 h-5 text-secondary" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
                     </div>
-                    <input type="text" v-model="seacrhCity" class="bg-gray100 border border-primary text-secondary text-sm font-normal radius-lg focus:ring-primary focus:border-primary block w-full pl-10 p-2.5" placeholder="Search City" required>
+                    <input type="text" v-model="searchValue" class="bg-gray100 border border-primary text-secondary text-sm font-normal radius-lg focus:ring-primary focus:border-primary block w-full pl-10 p-2.5" placeholder="Search City" required>
                 </div>
-                <button type="button" @click="" class="p-2.5 ml-2 text-sm font-medium text-white bg-primary radius-lg border border-primary hover:brightness-95 focus:ring-2 focus:outline-none focus:ring-primary">
+                <button type="button" @click="searchHandle" class="p-2.5 ml-2 text-sm font-medium text-white bg-primary radius-lg border border-primary hover:brightness-95 focus:ring-2 focus:outline-none focus:ring-primary">
                     <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                 </button>
             </form>
@@ -19,8 +19,9 @@
 <script setup langs="ts">
 
     const store = useWeatherStore()
-    const {  fetchWeather } = storeToRefs(store);
-    let userLocation = '';
+    // const {  fetchWeather } = storeToRefs(store);
+    const {  fetchWeather } = store;
+    const searchValue = ref('');
 
     function getLocation() {
       if (navigator.geolocation) {
@@ -29,13 +30,21 @@
     }
 
     function setPosition(position) {
-        userLocation = position.coords.latitude+ ','+ position.coords.longitude
-        console.log(userLocation);
+        searchValue = position.coords.latitude+ ','+ position.coords.longitude
+        console.log(searchValue);
+    }
+
+    const searchHandle = () => {
+        fetchWeather(searchValue.value);
+        searchValue.value = '';
+        console.log(searchValue.value, 'Button');
     }
     
     onMounted(() => {
         getLocation();
+        fetchWeather(searchValue.value);
     });
+
 
 
 </script>
--- stores/weather.ts
+++ stores/weather.ts
@@ -1,6 +1,6 @@
 export const useWeatherStore = defineStore("weather", {
   state: () => ({
-    weatherKey: "f11d4cbdc4624d94986143412232903",
+    // weatherKey: "f11d4cbdc4624d94986143412232903",
     weatherData: [],
   }),
 
@@ -10,14 +10,21 @@ export const useWeatherStore = defineStore("weather", {
     async fetchWeather(region: any) {
       try {
         const { data }: any = await useFetch(
-          `https://api.weatherapi.com/v1/forecast.json?key=f11d4cbdc4624d94986143412232903&q=Istanbul&days=7&aqi=no&alerts=no`
+          `https://api.weatherapi.com/v1/forecast.json?key=f11d4cbdc4624d94986143412232903&q=${
+            region ? region : "London"
+          }&days=7&aqi=no&alerts=no`
         );
         if (data) {
           this.weatherData = data.value;
         }
+        console.log(region, "regi");
       } catch (error) {
         console.log(error);
       }
     },
   },
 });
+
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(useWeatherStore, import.meta.hot));
+}
